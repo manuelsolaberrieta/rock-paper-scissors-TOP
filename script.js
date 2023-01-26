c17c03d36f16cc163f72534e9d0029d8cf94f7cc
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2023-01-26 01:30:30 -0300

    Adjusted CSS, cleaned js with eslint

--- script.js
+++ script.js
@@ -1,67 +1,20 @@
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  return choices[Math.floor(Math.random() * 3)];
-}
 const playerPointsTag = document.querySelector("#player-points");
 const PCPointsTag = document.querySelector("#pc-points");
 const tiesTag = document.querySelector("#ties");
 const resultTag = document.querySelector("#result");
 
-function playRound(playerChoice, computerChoice) {
-  let playerPoints = parseInt(playerPointsTag.textContent);
-  let computerPoints = parseInt(PCPointsTag.textContent);
-  let ties = parseInt(tiesTag.textContent);
-  if (playerChoice == "rock") {
-    if (computerChoice == "rock") {
-      resultTag.textContent = "It's a tie!";
-      tiesTag.textContent = `${(ties += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else if (computerChoice == "paper") {
-      resultTag.textContent = "You lose! Paper beats Rock";
-      PCPointsTag.textContent = `${(computerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else {
-      resultTag.textContent = "You win! Rock beats Scissors";
-      playerPointsTag.textContent = `${(playerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    }
-  } else if (playerChoice == "paper") {
-    if (computerChoice == "paper") {
-      resultTag.textContent = "It's a tie!";
-      tiesTag.textContent = `${(ties += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else if (computerChoice == "rock") {
-      resultTag.textContent = "You win! Paper beats Rock";
-      playerPointsTag.textContent = `${(playerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else {
-      resultTag.textContent = "You lose! Scissors beats Paper";
-      PCPointsTag.textContent = `${(computerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    }
-  } else if (playerChoice == "scissors") {
-    if (computerChoice == "scissors") {
-      resultTag.textContent = "It's a tie!";
-      tiesTag.textContent = `${(ties += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else if (computerChoice == "rock") {
-      resultTag.textContent = "You lose! Rock beats Scissors";
-      PCPointsTag.textContent = `${(computerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    } else {
-      resultTag.textContent = "You win! Scissors beats Paper";
-      playerPointsTag.textContent = `${(playerPoints += 1)}`;
-      calculatePoints(playerPoints, computerPoints);
-    }
-  }
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
-
-function calculatePoints(pP, cP) {
-  if (pP < 5 && cP < 5) {
-    return;
-  } else {
-    defineWinner(pP, cP);
+function showMessage(msg) {
+  const winner = document.createElement("h1");
+  winner.textContent = msg;
+  const gameTags = document.getElementsByClassName("game");
+  for (let i = 0; i < gameTags.length; i += 1) {
+    gameTags[i].style.display = "none";
   }
+  document.getElementById("result").textContent = msg;
 }
 function defineWinner(pP, cP) {
   let msg = "";
@@ -74,15 +27,62 @@ function defineWinner(pP, cP) {
   }
   showMessage(msg);
 }
-function showMessage(msg) {
-  const winner = document.createElement("h1");
-  winner.textContent = msg;
-  const gameTags = document.getElementsByClassName("game");
-  for (let i = 0; i < gameTags.length; i++) {
-    gameTags[i].style.display = "none";
+function calculatePoints(pP, cP) {
+  if (pP < 5 && cP < 5) {
+    /* empty */
+  } else {
+    defineWinner(pP, cP);
   }
-  document.getElementById("result").textContent = msg;
 }
+function playRound(playerChoice, computerChoice) {
+  let playerPoints = parseInt(playerPointsTag.textContent, 10);
+  let computerPoints = parseInt(PCPointsTag.textContent, 10);
+  let ties = parseInt(tiesTag.textContent, 10);
+  if (playerChoice === "rock") {
+    if (computerChoice === "rock") {
+      resultTag.textContent = "It's a tie!";
+      tiesTag.textContent = `${(ties += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else if (computerChoice === "paper") {
+      resultTag.textContent = "You lose! Paper beats Rock";
+      PCPointsTag.textContent = `${(computerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else {
+      resultTag.textContent = "You win! Rock beats Scissors";
+      playerPointsTag.textContent = `${(playerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "paper") {
+      resultTag.textContent = "It's a tie!";
+      tiesTag.textContent = `${(ties += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else if (computerChoice === "rock") {
+      resultTag.textContent = "You win! Paper beats Rock";
+      playerPointsTag.textContent = `${(playerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else {
+      resultTag.textContent = "You lose! Scissors beats Paper";
+      PCPointsTag.textContent = `${(computerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "scissors") {
+      resultTag.textContent = "It's a tie!";
+      tiesTag.textContent = `${(ties += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else if (computerChoice === "rock") {
+      resultTag.textContent = "You lose! Rock beats Scissors";
+      PCPointsTag.textContent = `${(computerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    } else {
+      resultTag.textContent = "You win! Scissors beats Paper";
+      playerPointsTag.textContent = `${(playerPoints += 1)}`;
+      calculatePoints(playerPoints, computerPoints);
+    }
+  }
+}
+
 const selectionButtons = document.querySelectorAll(".selection");
 selectionButtons.forEach((element) => {
   element.addEventListener("click", () =>
